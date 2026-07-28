e513867ce19cd460bfffd3321a17ab046141cfbb
Stanley Winata <winatastanley355@gmail.com>
2026-07-28 14:09:40 +0700

    feat: Update middleware

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -1,9 +1,5 @@
-// mod axum_response;
 mod request;
 mod response;
-// mod session;
 
-// pub use axum_response::{AxumResponse, JsonFindResponse, JsonResponse};
-// pub use session::Session;
 pub use request::RequestMiddleware;
 pub use response::{AxumResponse, DataAndPagination, JsonResponse, Pagination};
